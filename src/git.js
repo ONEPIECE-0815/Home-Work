9bc772f89857b026b322b85ae269b3f082447bae
ONEPIECE-0815 <56755271+ONEPIECE-0815@users.noreply.github.com>
2020-01-10 13:07:30 +0800

    0110

--- src/git.js
+++ src/git.js
@@ -3,13 +3,18 @@
 git status //查看状态
 git add . //本地仓库添加 全部文件
 git commit -m '备注信息' //commit是提交,-m是备注信息
+
 git branch login //创建分支,分支名是login
 git checkout login //切换到login分支
 git checkout master //切换到主线
 git rm --cached 文件名 //删除本地已经上添加的文件
 git merge 分支名 //合并分支到主线(需要在主线下输入命令)
+
+
 git remote //查看有没有对应的地址
 git remote add origin https://github.com/ONEPIECE-0815/Home-Work.git //添加GitHub仓库地址
+
+
 git push -u origin master //
 git clone 仓库地址:https://github.com/ONEPIECE-0815/Home-Work.git  //先在目标文件夹地址cmd,把GitHub代码复制到本地
 git clone -b 分支名 地址:https://github.com/ONEPIECE-0815/Home-Work.git  //先在目标文件夹地址cmd,把GitHub代码复制到本地
